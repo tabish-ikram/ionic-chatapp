e6ae342045cee7d4a5e62c57d123c32ff4b86986
Tabish-Ikram <tabishikram1947@gmail.com>
2015-08-31 21:17:45 -0700

    Add routing

--- chatApp/www/js/app.js
+++ chatApp/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('myApp', ['ionic','ui.router'])
+angular.module('myApp', ['ionic'])
 
 /*
 .run(function($ionicPlatform) {
@@ -21,8 +21,34 @@ angular.module('myApp', ['ionic','ui.router'])
 */
 
 .config(function($stateProvider, $urlRouterProvider){
-    $urlRouterProvider.otherwise();
+    $urlRouterProvider.otherwise("/login");
 
     $stateProvider
-      .state()
+      .state("login",{
+        url: "/login",
+        templateUrl: "/views/login.html"
+      })
+      .state("tabs",{
+        url: "/tabs",
+        abstract: true,
+        templateUrl: "/views/tabs.html"
+      })
+      .state("tabs.chat",{
+        url: "/chat",
+        views:{
+          'chat-tab':{
+            templateUrl: "/views/chat.html"
+          }
+        }
+
+      })
+      .state("tabs.friends",{
+        url: "/friends",
+        views:{
+          'friends-tab':{
+            templateUrl: "/views/friends.html"
+          }
+        }
+      })
+
   })
