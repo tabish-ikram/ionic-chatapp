3a392762c44ba33aa15f861a168fb13287716532
Tabish-Ikram <tabishikram1947@gmail.com>
2015-10-20 21:49:57 -0700

    implement friends functionality

--- chatApp/www/js/app.js
+++ chatApp/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('myApp', ['ionic'])
+angular.module('myChat', ['ionic','ui.router','firebase','monospaced.elastic','angularMoment'])
 
 /*
 .run(function($ionicPlatform) {
@@ -26,18 +26,21 @@ angular.module('myApp', ['ionic'])
     $stateProvider
       .state("login",{
         url: "/login",
-        templateUrl: "/views/login.html"
+        templateUrl: "views/login.html",
+        controller : "loginController as logCtrl"
       })
       .state("tabs",{
         url: "/tabs",
         abstract: true,
-        templateUrl: "/views/tabs.html"
+        templateUrl: "views/tabs.html"
+
       })
       .state("tabs.chat",{
         url: "/chat",
         views:{
           'chat-tab':{
-            templateUrl: "/views/chat.html"
+            templateUrl: "views/chat.html",
+            controller: "UserMessagesCtrl"
           }
         }
 
@@ -46,9 +49,17 @@ angular.module('myApp', ['ionic'])
         url: "/friends",
         views:{
           'friends-tab':{
-            templateUrl: "/views/friends.html"
+            templateUrl: "views/friends.html",
+            controller: "friendsCtrl"
           }
         }
       })
 
   })
+
+  /*.config(function($cordovaFacebookProvider) {
+    var appID = 1101475826544114;
+    var version = "v2.0"; // or leave blank and default is v2.0
+    $cordovaFacebookProvider.browserInit(appID, version);
+  })
+*/
